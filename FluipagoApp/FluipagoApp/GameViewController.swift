64aca37b026d2d92a6ec1baef902500139208e1a
SassyGizmo <juniorvaliente530@gmail.com>
2025-02-26 15:44:18 -0300

    Alerta popup

--- FluipagoApp/FluipagoApp/GameViewController.swift
+++ FluipagoApp/FluipagoApp/GameViewController.swift
@@ -16,17 +16,33 @@ class GameViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var pointsLabel: UILabel!
+    @IBOutlet weak var popup: UIView!
+    @IBOutlet weak var popupLabel: UILabel!
+    @IBOutlet weak var puntajePopup: UILabel!
+    @IBOutlet weak var popupButton: UIButton!
     
     var timer : Timer?
     var timerLeft = 30
     var puntosPorClick = 0
     
+    private let popupColor = UIColor(red: 216/255, green: 64/255, blue: 64/255, alpha: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         clickButton.isHidden = true
+        popupHidden()
+        
+        
         timerBox.layer.cornerRadius = 20
         timerBox.layer.masksToBounds = true
+        
+        popup.layer.cornerRadius = 20
+        popup.layer.masksToBounds = true
+        popup.layer.borderWidth = 2
+        popup.layer.borderColor = popupColor.cgColor
+        
         
         //clickButton.center = CGPoint(x: gameField.bounds.midX, y: gameField.bounds.midY)
                 
@@ -45,6 +61,19 @@ class GameViewController: UIViewController {
     
     
     
+    func popupHidden() {
+            popup.isHidden = true
+            popupLabel.isHidden = true
+            puntajePopup.isHidden = true
+            popupButton.isHidden = true
+    }
+    
+    func popupShow() {
+            popup.isHidden = false
+            popupLabel.isHidden = false
+            puntajePopup.isHidden = false
+            popupButton.isHidden = false
+    }
     
     func startTimer() {
         timerLabel.text = "\(timerLeft)"
@@ -57,6 +86,9 @@ class GameViewController: UIViewController {
             timerLeft -= 1
             timerLabel.text = "\(timerLeft)"
         } else {
+            puntajePopup.text = "Puntaje: \(puntosPorClick)"
+            popupShow()
+            clickButton.isHidden = true
             timer?.invalidate()
             timerLabel.text = "Fin"
             
@@ -81,6 +113,7 @@ class GameViewController: UIViewController {
     }
     
     @IBAction func startButtonClick(_ sender: Any) {
+        popupHidden()
         clickButtonTapped()
         puntosPorClick = 0
         pointsLabel.text = "0"
@@ -93,7 +126,15 @@ class GameViewController: UIViewController {
     @IBAction func stopButtonClick(_ sender: Any) {
         stopTimer()
         clickButton.isHidden = true
+        popupShow()
+        puntajePopup.text = "Puntaje: \(puntosPorClick)"
         
+    }
+    @IBAction func popupButtonTapped(_ sender: Any) {
+        popupHidden()
+        puntosPorClick = 0
+        timerLeft = 0
+        timerLabel.text = "\(puntosPorClick)"
     }
     
 }
